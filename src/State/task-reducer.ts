509882c346c7d16d042dbea8b52f0de57a607a17
Pavel <pavel07007@gmail.com>
2023-01-14 14:25:34 +0300

    add reducer and test for add task

--- src/State/task-reducer.ts
+++ src/State/task-reducer.ts
@@ -1,12 +1,16 @@
 import {TaskObjType} from "../App";
+import {v1} from "uuid";
 
 export const taskReducer =(state:TaskObjType, action:taskReducerActionType)=>{
     switch (action.type){
         case "DELETE-TASK": return {...state, [action.ListId]: state[action.ListId].filter(el => el.id !== action.taskId)}
+        case 'ADD-TASK': return {...state, [action.listId]: [...state[action.listId], {id: v1(), title: action.title, isDone: false}]}
         default:return state
     }
 }
-export type taskReducerActionType = deleteTaskACType
+export type taskReducerActionType = deleteTaskACType|addTaskACType
 
 type deleteTaskACType =ReturnType<typeof deleteTaskAC>
+type addTaskACType =ReturnType<typeof addTaskAC>
 export const deleteTaskAC = (ListId: string, taskId: string)=> {return{type:"DELETE-TASK", ListId,taskId} as const}
+export const addTaskAC =(listId:string, title:string)=>{return{type:"ADD-TASK",listId,title}as const }
--- src/State/tasks-reducer.test.ts
+++ src/State/tasks-reducer.test.ts
@@ -1,4 +1,4 @@
-import {deleteTaskAC, taskReducer} from "./task-reducer";
+import {addTaskAC, deleteTaskAC, taskReducer} from "./task-reducer";
 import {v1} from "uuid";
 import {TaskObjType} from "../App";
 let todolistID1 = v1()
@@ -12,7 +12,6 @@ beforeEach(()=>{
             {id: '1', title: 'HTML&CSS', isDone: true},
             {id: '2', title: 'JS', isDone: true},
             {id: '3', title: 'ReactJS', isDone: false},
-
         ],
         [todolistID2]: [
             {id: '1', title: 'Rest API', isDone: true},
@@ -25,4 +24,10 @@ test(('must delete task from tasks obj'),()=>{
     const result = taskReducer(tasks, deleteTaskAC(todolistID1,'1' ))
     expect(result[todolistID1][0].title).toBe('JS')
     expect(result[todolistID2][0].title).toBe('Rest API')
+})
+test(('must add task for specific list'),()=>{
+    const result = taskReducer(tasks, addTaskAC(todolistID2, 'title'))
+    expect(result[todolistID1].length).toBe(3)
+    expect(result[todolistID2].length).toBe(3)
+    expect(result[todolistID2][2].title).toBe('title')
 })

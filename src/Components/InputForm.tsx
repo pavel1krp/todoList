28d0860be1db2d74e910e8e069b78518d7a64dea
Pavel <pavel07007@gmail.com>
2023-01-14 13:03:45 +0300

    ann error for wrong value in InputForm.tsx

--- src/Components/InputForm.tsx
+++ src/Components/InputForm.tsx
@@ -6,17 +6,26 @@ type InputFormPropsType = {
 
 export const InputForm = (props:InputFormPropsType) => {
     const {addTask, ...restProps}= props
+    const [error, serError]= useState('')
     const [title, setTitle] = useState('');
     const changeInputValue = (e:ChangeEvent<HTMLInputElement>)=>{
+        serError('')
         setTitle(e.currentTarget.value)
     }
     const addTextHandler =()=>{
-        addTask(title)
+        if(title.trim()!==''){
+            addTask(title)
+        }
+       else{
+           serError('Wrong value!')
+        }
+       setTitle('')
     }
     return (
         <div>
             <input onChange={changeInputValue} value={title}/>
             <button onClick={addTextHandler}>+</button>
+            {error? <div>{error}</div>: null}
         </div>
     );
 };

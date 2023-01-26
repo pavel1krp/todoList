0e51e15b2d6cca5a1d7511dc1198ce9f18e2944b
Pavel <pavel07007@gmail.com>
2023-01-26 17:39:39 +0300

    add storybook

---
+++ src/Components/Task.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Task} from "./Task";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {Provider} from "react-redux";
+import {store} from "../State/store";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'TodoList/Task',
+  component: Task,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args:{
+    taskId:'1',
+    isDone:false,
+    title:'Task',
+    listId:'1'
+  },
+} as ComponentMeta<typeof Task>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Task> = (args) =><Provider  store={store}><Task {...args} /></Provider> ;
+
+export const UncheckedTask  = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+

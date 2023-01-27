68cb7015b8cd38e841670b3746c18e67b701eb66
Pavel <pavel07007@gmail.com>
2023-01-27 23:02:57 +0300

    r

--- src/State/task-reducer.ts
+++ src/State/task-reducer.ts
@@ -16,12 +16,21 @@ export const taskReducer =(state:TaskObjType = initialState, action:taskReducerA
 export type taskReducerActionType = deleteTaskACType|addTaskACType|changeIdDoneACType|addTodoListAcType|changeTaskTitleACType
 
 type deleteTaskACType =ReturnType<typeof deleteTaskAC>
+
 type addTaskACType =ReturnType<typeof addTaskAC>
+
 type  changeIdDoneACType=ReturnType<typeof changeIdDoneAC>
+
 export type  addTodoListAcType=ReturnType<typeof addTodoListAc>
+
 export type  changeTaskTitleACType=ReturnType<typeof changeTaskTitleAC>
+
 export const changeTaskTitleAC =(listId:string, taskId:string,title:string)=>{return{type:'CHANGE-TITLE',listId,taskId,title} as const}
+
 export const deleteTaskAC = (ListId: string, taskId: string)=> {return{type:"DELETE-TASK", ListId,taskId} as const}
+
 export const addTaskAC =(listId:string, title:string)=>{return{type:"ADD-TASK",listId,title}as const }
+
 export const changeIdDoneAC =(todoListId: string, taskId: string, done: boolean)=>{return{type:"CHANGE-IS-DONE",todoListId,taskId,done}as const }
+
 export const addTodoListAc =(title:string)=>{return{type:"ADD-TODO-LIST",title, listId:v1()}as const }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {Provider} from "react-redux";
-import {store} from "./State/store";
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import {Provider} from "react-redux";
+// import {store} from "./State/store";
+//
+//
+// const root = ReactDOM.createRoot(
+//     document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//     <React.StrictMode>
+//         <Provider store={store}>
+//             <App/>
+//         </Provider>
+//     </React.StrictMode>
+// );
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//
+import React, {useMemo, useState} from 'react'
+import ReactDOM from 'react-dom'
 
+type ButtonType = {
+    id: number
+    title: string
+    forAdminOnly: boolean
+}
+const buttons: ButtonType[] = [
+    {id: 1, title: 'delete', forAdminOnly: true},
+    {id: 2, title: 'update', forAdminOnly: true},
+    {id: 3, title: 'create', forAdminOnly: false},
+]
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </React.StrictMode>
-);
+export const App = ({isAdmin}: { isAdmin: boolean }) => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    const [seconds, setSeconds] = useState(0)
 
+    const increaseSeconds = () => setSeconds(seconds + 10)
+
+    const correctButtons = useMemo(() => {
+        return buttons.filter(b => isAdmin ? true : !b.forAdminOnly)
+    }, [isAdmin])
+
+    return <>
+        <ButtonsPanel buttons={correctButtons}/>
+        <div>
+            <p>
+                <b>Секунды: {seconds}</b>
+            </p>
+            <button onClick={increaseSeconds}>
+                Увеличить на 10 секунд
+            </button>
+        </div>
+    </>
+}
+
+const ButtonsPanel = React.memo((props: { buttons: Array<ButtonType> }) => {
+    console.log('Render ButtonsPanel')
+    return (
+        <div style={{marginBottom: '15px'}}>
+            <div style={{marginBottom: '15px'}}>
+                <b>Панель с кнопками</b>
+            </div>
+            <div>
+                {props.buttons.map(b => <button key={b.id}>{b.title}</button>)}
+            </div>
+        </div>
+    )
+})
+
+ReactDOM.render(<App isAdmin={true}/>, document.getElementById('root'))

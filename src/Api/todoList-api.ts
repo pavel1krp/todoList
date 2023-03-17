2ca8917b047d4ccac4702f048ee4d6eaec209405
Pavel <pavel07007@gmail.com>
2023-03-17 23:24:37 +0300

    add instance and getTodo method

--- src/Api/todoList-api.ts
+++ src/Api/todoList-api.ts
@@ -1,20 +1,22 @@
 import axios from 'axios'
 
-const settings = {
-    withCredentials: true,
-    headers: {
-        // Не забываем заменить API-KEY на собственный
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41',
-    },
-}
-
+const instance = axios.create({
+    baseURL:'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials:true,
+    headers:{
+        'API-KEY': '1136952c-a2c5-4464-80fa-484fab36a6a8',
+    }
+})
 export const todolistAPI = {
     updateTodolist(todolistId: string, title: string) {
-        const promise = axios.put(
-            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
+        const promise = instance.put(
+            `todo-lists/${todolistId}`,
             { title: title },
-            settings
         )
         return promise
     },
+    getTodoLists(){
+        const promise = instance.get('todo-lists')
+        return promise
+    }
 }
--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -5,6 +5,7 @@ import {EditableSpan} from "./EditableSpan";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import {useDispatch} from "react-redux";
+import {todolistAPI} from "../Api/todoList-api";
 
 type PropsTaskType ={
     listId:string

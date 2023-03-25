6bb7c5640a1c76ca3a6082a69c33938b37906197
Pavel <pavel07007@gmail.com>
2023-03-25 09:12:44 +0300

    change type in api

--- src/Api/todoList-api.ts
+++ src/Api/todoList-api.ts
@@ -12,18 +12,36 @@ export type ResponseType<D> = {
     fieldsErrors: Array<string>
     data:D
 }
+export enum TaskStatuses {
+    New = 0,
+    InProgress = 1,
+    Completed = 2,
+    Draft = 3
+}
+export enum TaskPriorities {
+    Low = 0,
+    Middle = 1,
+    Hi = 2,
+    Urgently = 3,
+    Later = 4
+}
 export type TasksType = {
     description: string
     title: string
     completed: boolean
-    status: integer
-    priority: integer
-    startDate: datetime
-    deadline: datetime
+    status: TaskStatuses
+    priority: TaskPriorities
+    startDate: string
+    deadline: string
     id: string
     todoListId: string
-    order: integer
-    addedDate: datetime
+    order: number
+    addedDate: string
+}
+type GetTasksResponse = {
+    error: string | null
+    totalCount: number
+    items: TasksType[]
 }
 
 

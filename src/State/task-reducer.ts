5f8caa5fb8457e28bcb21adfd12332e5b7e32ce1
Pavel <pavel07007@gmail.com>
2023-06-08 16:29:42 +0300

    add setTask reducer

--- src/State/task-reducer.ts
+++ src/State/task-reducer.ts
@@ -6,7 +6,7 @@ import {TasksType, todolistAPI} from "../Api/todoList-api";
 
 const initialState: TaskObjType = {}
 
-export const taskReducer = (state: TaskObjType = initialState, action: taskReducerActionType): TaskObjType => {
+export const taskReducer = (state: TaskObjType = initialState, action: taskReducerActionType): TaskObjType|any => {
     switch (action.type) {
         case "DELETE-TASK":
             return {...state, [action.ListId]: state[action.ListId].filter(el => el.id !== action.taskId)}
@@ -34,15 +34,18 @@ export const taskReducer = (state: TaskObjType = initialState, action: taskReduc
                 } : el)
             }
         case 'SET-LIST':
-            // const copyState = {...state}
-            // action.todoLists.forEach((el) => {
-            //     copyState[el.id] = []
-            // })
-            return copyState
-        case "SET-TASKS":
             const copyState = {...state}
-
+            action.todoLists.forEach((el) => {
+                copyState[el.id] = []
+            })
             return copyState
+        case "SET-TASKS": {
+            return {
+                ...state,
+                [action.todoListId]: action.tasks
+            }
+        }
+
         default:
             return state
     }
@@ -52,7 +55,7 @@ export type taskReducerActionType = deleteTaskACType |
     changeIdDoneACType |
     addTodoListAcType |
     changeTaskTitleACType |
-    setTodoListAcType|
+    setTodoListAcType |
     setTAsksACType
 
 type deleteTaskACType = ReturnType<typeof deleteTaskAC>
@@ -62,14 +65,24 @@ export type  addTodoListAcType = ReturnType<typeof addTodoListAc>
 export type  changeTaskTitleACType = ReturnType<typeof changeTaskTitleAC>
 export type setTAsksACType = ReturnType<typeof setTAsksAC>
 
-export const changeTaskTitleAC = (listId: string, taskId: string, title: string) => ({type: 'CHANGE-TITLE', listId, taskId, title}) as const
-export const deleteTaskAC = (ListId: string, taskId: string) => ( {type: "DELETE-TASK", ListId, taskId}) as const
+export const changeTaskTitleAC = (listId: string, taskId: string, title: string) => ({
+    type: 'CHANGE-TITLE',
+    listId,
+    taskId,
+    title
+}) as const
+export const deleteTaskAC = (ListId: string, taskId: string) => ({type: "DELETE-TASK", ListId, taskId}) as const
 export const addTaskAC = (listId: string, title: string) => ({type: "ADD-TASK", listId, title}) as const
-export const changeIdDoneAC = (todoListId: string, taskId: string, done: boolean) => ({type: "CHANGE-IS-DONE", todoListId, taskId, done}) as const
-export const addTodoListAc = (title: string) =>  ({type: "ADD-TODO-LIST", title, listId: v1()}) as const
-export const setTAsksAC = (tasks: TasksType[], todoListId:string) =>  ({type: "SET-TASKS", tasks, todoListId}) as const
+export const changeIdDoneAC = (todoListId: string, taskId: string, done: boolean) => ({
+    type: "CHANGE-IS-DONE",
+    todoListId,
+    taskId,
+    done
+}) as const
+export const addTodoListAc = (title: string) => ({type: "ADD-TODO-LIST", title, listId: v1()}) as const
+export const setTAsksAC = (tasks: TasksType[], todoListId: string) => ({type: "SET-TASKS", tasks, todoListId}) as const
 
-export const getTasksTC = (todoListId:string)=>(dispatch:Dispatch)=>{
+export const getTasksTC = (todoListId: string) => (dispatch: Dispatch) => {
     todolistAPI.getTasks(todoListId)
-        .then(res=> dispatch(setTAsksAC(res.data.items, todoListId)) )
+        .then(res => dispatch(setTAsksAC(res.data.items, todoListId)))
 }
